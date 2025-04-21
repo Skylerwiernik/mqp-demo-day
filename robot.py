bdd565a38aecbdffd25b506a5c0e83e2800bb807
Skyler <skylerwiernik@gmail.com>
2025-04-21 10:48:04 -0400

    Properly name blue led

--- robot.py
+++ robot.py
@@ -7,6 +7,6 @@ from target import LEDSequence
 class PiBot(Robot):
     def __init__(self):
         self.green_led = LED("Green LED", 23)
-        self.blue_led = LED("Red LED", 24)
+        self.blue_led = LED("Blue LED", 24)
         self.target = LEDSequence(self)
         super().__init__([self.green_led, self.blue_led], [self.target])

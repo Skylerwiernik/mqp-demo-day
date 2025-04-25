1ecc005c28f1ab6f2c9f5d3c2683e02690e39967
Skyler <skylerwiernik@gmail.com>
2025-04-24 21:03:46 -0400

    Revert "Stuff for new stdout/stderr syntax"
    This reverts commit 47427afdffe6b529be9db031bfb6d07f0997cbda.

--- target.py
+++ target.py
@@ -13,13 +13,11 @@ class LEDSequence(Target):
         super().__init__("LED Sequence", robot)
 
 
-    def _run(self):
+    def _run(self) -> threading.Thread:
         self.running = True
-        self.robot.green_led.brightness.value = 0.5
-        self.robot.blue_led.brightness.value = 0.5
-        while self.running:
-            self.__toggle_leds()
-            sleep(2)
+        self.thread = threading.Thread(target=self.__worker)
+        self.thread.start()
+        return self.thread
 
     async def _shutdown(self, beat: Callable[[], None]):
         if self.thread:
@@ -27,6 +25,13 @@ class LEDSequence(Target):
             self.thread.join()
             self.robot.blue_led.enabled.value = False
             self.robot.green_led.enabled.value = False
+
+    def __worker(self):
+        self.robot.green_led.brightness.value = 0.5
+        self.robot.blue_led.brightness.value = 0.5
+        while self.running:
+            self.__toggle_leds()
+            sleep(2)
 
 
     def __toggle_leds(self):

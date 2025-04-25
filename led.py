8cfbf2b9f893def75d18202a24a5f07183566123
Skyler <skylerwiernik@gmail.com>
2025-04-24 20:13:06 -0400

    Revert "deinit led device"
    This reverts commit 65844b08ee51e602a0b88beb3aacc7560ee16668.

--- led.py
+++ led.py
@@ -23,10 +23,6 @@ class LED(Device):
 
         super().__init__([self.brightness, self.enabled], self.graphic_cell)
 
-
-    def __del__(self):
-        self.pwm.stop()
-
     def __set_brightness(self, duty_cycle: float):
         self.pwm.ChangeDutyCycle(duty_cycle)
 
